f7b3e9477b9717fb2db051f6130f7f9d8ea18703
Stephen <smithwithatypo@gmail.com>
2023-03-23 14:57:52 -0500

    abstracted data augmentation layer

--- train.py
+++ train.py
@@ -9,9 +9,7 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 start_time = time.time()
-tf.debugging.set_log_device_placement(True)
-
-# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+# tf.debugging.set_log_device_placement(True)  # shows if GPU or CPU is used
 
 batch_size = 32
 img_height = 64
@@ -46,25 +44,38 @@ val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 num_classes = len(class_names)
 
+# data_augmentation = keras.Sequential([
+#     layers.RandomFlip("horizontal",
+#                       input_shape=(img_height,
+#                                    img_width,
+#                                    3)),
+#     layers.RandomRotation(0.1),
+#     layers.RandomZoom(0.1)
+# ]
+# )
+
 model = Sequential([
-    layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+    # data_augmentation,
+    layers.Rescaling(1./255),
     layers.Conv2D(16, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
     layers.Conv2D(32, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
     layers.Conv2D(64, 3, padding='same', activation='relu'),
     layers.MaxPooling2D(),
+    layers.Dropout(0.2),
     layers.Flatten(),
     layers.Dense(128, activation='relu'),
-    layers.Dense(num_classes)
+    layers.Dense(num_classes, name="outputs")
 ])
 
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                  from_logits=True),
               metrics=['accuracy'])
 
 epochs = 10
-model.fit(
+history = model.fit(
     train_ds,
     validation_data=val_ds,
     epochs=epochs
